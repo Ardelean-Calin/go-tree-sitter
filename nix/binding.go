428762fb5c50dc5c15f225af130e5fc29ee3ce81
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2024-03-19 15:50:46 +0200

    Added Nix bindings.

---
+++ nix/binding.go
@@ -0,0 +1,15 @@
+package bash
+
+//#include "tree_sitter/parser.h"
+//TSLanguage *tree_sitter_nix();
+import "C"
+import (
+	"unsafe"
+
+	sitter "github.com/smacker/go-tree-sitter"
+)
+
+func GetLanguage() *sitter.Language {
+	ptr := unsafe.Pointer(C.tree_sitter_nix())
+	return sitter.NewLanguage(ptr)
+}
